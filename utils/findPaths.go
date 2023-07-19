e2dd2591999dc9eff931b08347f3069d6d9f0f3d
serignmbaye <barsaliou1998@gmail.com>
2023-07-19 17:17:16 +0000

    fix: if there is no path

--- utils/findPaths.go
+++ utils/findPaths.go
@@ -1,30 +1,6 @@
 package utils
 
-import (
-	"lemin/model"
-)
-
-func findPathsRecursive(currentRoom string, path []string, visited map[string]bool, tunnels []model.Link, endRoom string, paths *[][]string) {
-	visited[currentRoom] = true
-
-	path = append(path, currentRoom)
-
-	if currentRoom == endRoom {
-		newPath := make([]string, len(path))
-		copy(newPath, path)
-		*paths = append(*paths, newPath)
-	} else {
-		for _, tunnel := range tunnels {
-			if tunnel.From == currentRoom && !visited[tunnel.To] {
-				findPathsRecursive(tunnel.To, append([]string{}, path...), visited, tunnels, endRoom, paths)
-			} else if tunnel.To == currentRoom && !visited[tunnel.From] {
-				findPathsRecursive(tunnel.From, append([]string{}, path...), visited, tunnels, endRoom, paths)
-			}
-		}
-	}
-
-	visited[currentRoom] = false
-}
+import "lemin/model"
 
 func FindPaths(antFarm model.AntFarm) []model.Path {
 	visited := make(map[string]bool)
--- utils/moveAnts.go
+++ utils/moveAnts.go
@@ -12,49 +12,51 @@ type antPosition struct {
 }
 
 func MoveAnts(paths []model.Path, numberOfAnts int) {
-	currentPath := 0
-	antPositions := make(map[string]*antPosition)
-	for i := 0; i < numberOfAnts; i++ {
-		nextPath := (currentPath + 1) % len(paths)
-		ant := model.Ant{
-			Name: fmt.Sprintf("%d", i+1),
-		}
-		currentPathValue := len(paths[currentPath].Ants) + len(paths[currentPath].Rooms)
-		nextPathValue := len(paths[nextPath].Ants) + len(paths[nextPath].Rooms)
+	if len(paths) != 0 {
+		currentPath := 0
+		antPositions := make(map[string]*antPosition)
+		for i := 0; i < numberOfAnts; i++ {
+			nextPath := (currentPath + 1) % len(paths)
+			ant := model.Ant{
+				Name: fmt.Sprintf("%d", i+1),
+			}
+			currentPathValue := len(paths[currentPath].Ants) + len(paths[currentPath].Rooms)
+			nextPathValue := len(paths[nextPath].Ants) + len(paths[nextPath].Rooms)
 
-		if currentPathValue > nextPathValue {
-			paths[nextPath].Ants = append(paths[nextPath].Ants, ant)
-			currentPath = nextPath
-		} else {
-			paths[currentPath].Ants = append(paths[currentPath].Ants, ant)
-		}
-		i := len(paths[currentPath].Ants) - 1
-		if i != 0 {
-			i *= -1
-		}
-		antPositions[ant.Name] = &antPosition{currentPath, i}
-	}
-
-	moves := ""
-	for {
-		finished := true
-		for antName, pos := range antPositions {
-			if pos.roomIdx >= 0 && pos.roomIdx < len(paths[pos.pathIdx].Rooms) {
-				room := paths[pos.pathIdx].Rooms[pos.roomIdx]
-				moves += fmt.Sprintf("L%s-%s ", antName, room.Name)
-				pos.roomIdx++
-				finished = false
+			if currentPathValue > nextPathValue {
+				paths[nextPath].Ants = append(paths[nextPath].Ants, ant)
+				currentPath = nextPath
 			} else {
-				antPositions[antName].roomIdx++
+				paths[currentPath].Ants = append(paths[currentPath].Ants, ant)
+			}
+			i := len(paths[currentPath].Ants) - 1
+			if i != 0 {
+				i *= -1
+			}
+			antPositions[ant.Name] = &antPosition{currentPath, i}
+		}
+
+		moves := ""
+		for {
+			finished := true
+			for antName, pos := range antPositions {
+				if pos.roomIdx >= 0 && pos.roomIdx < len(paths[pos.pathIdx].Rooms) {
+					room := paths[pos.pathIdx].Rooms[pos.roomIdx]
+					moves += fmt.Sprintf("L%s-%s ", antName, room.Name)
+					pos.roomIdx++
+					finished = false
+				} else {
+					antPositions[antName].roomIdx++
+				}
+			}
+			if finished {
+				break
+			} else {
+				moves += "\n"
 			}
 		}
-		if finished {
-			break
-		} else {
-			moves += "\n"
-		}
-	}
 
-	fmt.Println(moves)
-	fmt.Println("🐜🐜 We have :", len(strings.Split(moves, "\n"))-1, " turns")
+		fmt.Println(moves)
+		fmt.Println("🐜🐜 We have :", len(strings.Split(moves, "\n"))-1, " turns")
+	}
 }
