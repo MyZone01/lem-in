000f6280c4e6c349ad2d680999d68307690e35d1
serignmbaye <barsaliou1998@gmail.com>
2023-07-20 19:03:26 +0000

    fix: avoid comparing the start + end rooms when finding an already taken room inside a new path

--- utils/findPaths.go
+++ utils/findPaths.go
@@ -1,63 +1,63 @@
 package utils
 
-import "lemin/model"
+import (
+	"lemin/model"
+)
 
 func FindPaths(antFarm model.AntFarm) []model.Path {
-    visited := make(map[string]bool)
-    paths := [][]string{}
+	visited := make(map[string]bool)
+	paths := [][]string{}
 
-    // Recursive function to find paths
-    var findPathsRecursive func(currentRoom string, path []string)
-    findPathsRecursive = func(currentRoom string, path []string) {
-        visited[currentRoom] = true
-        path = append(path, currentRoom)
-        if currentRoom == antFarm.End.Name {
-            newPath := make([]string, len(path))
-            copy(newPath, path)
+	// Recursive function to find paths
+	var findPathsRecursive func(currentRoom string, path []string)
+	findPathsRecursive = func(currentRoom string, path []string) {
+		visited[currentRoom] = true
+		path = append(path, currentRoom)
+		if currentRoom == antFarm.End.Name {
+			newPath := make([]string, len(path))
+			copy(newPath, path)
 
-            // Check if newPath shares any room with existing paths
-            newPathHasSharedRoom := false
-            for _, existingPath := range paths {
-                for _, room := range newPath {
-                    if contains(existingPath, room) {
-                        newPathHasSharedRoom = true
-                        break
-                    }
-                }
-                if newPathHasSharedRoom {
-                    break
-                }
-            }
+			// Check if newPath shares any room with existing paths
+			newPathHasSharedRoom := false
+			for _, existingPath := range paths {
+				for _, room := range newPath {
+					if contains(existingPath, room) {
+						newPathHasSharedRoom = true
+						break
+					}
+				}
+				if newPathHasSharedRoom {
+					break
+				}
+			}
 
-            // Only append newPath if it does not share any room with existing paths
-            if !newPathHasSharedRoom {
-                paths = append(paths, newPath)
-            }
-        } else {
-            for _, tunnel := range antFarm.Links {
-                if tunnel.From == currentRoom && !visited[tunnel.To] {
-                    findPathsRecursive(tunnel.To, path)
-                } else if tunnel.To == currentRoom && !visited[tunnel.From] {
-                    findPathsRecursive(tunnel.From, path)
-                }
-            }
-        }
-        visited[currentRoom] = false
-    }
+			// Only append newPath if it does not share any room with existing paths
+			if !newPathHasSharedRoom {
+				paths = append(paths, newPath)
+			}
+		} else {
+			for _, tunnel := range antFarm.Links {
+				if tunnel.From == currentRoom && !visited[tunnel.To] {
+					findPathsRecursive(tunnel.To, path)
+				} else if tunnel.To == currentRoom && !visited[tunnel.From] {
+					findPathsRecursive(tunnel.From, path)
+				}
+			}
+		}
+		visited[currentRoom] = false
+	}
 
-    findPathsRecursive(antFarm.Start.Name, []string{})
-    RangePaths(paths)
-    return StringToRoom(paths, antFarm)
+	findPathsRecursive(antFarm.Start.Name, []string{})
+	RangePaths(paths)
+	return StringToRoom(paths, antFarm)
 }
 
 // Contains checks if a slice contains a string
 func contains(slice []string, str string) bool {
-    for _, item := range slice {
-        if item == str {
-            return true
-        }
-    }
-    return false
+	for _, item := range slice {
+		if item != "start" && item != "end" && item == str {
+			return true
+		}
+	}
+	return false
 }
-
-
